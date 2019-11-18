d552a007e0d41306cab730fb3d54df72a5b779fb
Mark Halls <mark.halls@gmail.com>
2019-11-18 09:51:14 -0700

    Dispatch actions in login submit and remove unused imports

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -1,77 +1,70 @@
-import React from 'react';
-import { withFormik, Form, Field,  ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
-import {AuthWrapper} from './styled-components';
+import React from "react";
+import { withFormik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { connect } from "react-redux";
+import { AuthWrapper } from "./styled-components";
+import actions from "../actions";
 
-function LoginForm(props){
-    console.log(props);
-    
-    return (
-        <AuthWrapper>    
-            <div className = "login-form">
-                <Form>
-                    <label>
-                        <ErrorMessage
-                            name = "username"
-                            render = {msg=> <div className = "error">{msg}</div>}
-                            />
-                        Username
-                        <Field
-                            type = "text"
-                            name = "username"
-                            placeholder = "Username"
-                            />
-                    </label>
-                    <label>
-                        <ErrorMessage
-                            name = "password"
-                            render = {msg=> <div className = "error">{msg}</div>}
-                            />
-                        Password
-                        <Field
-                            type = "password"
-                            name = "password"
-                            placeholder = "Password"
-                            />
-                    </label>
+function LoginForm(props) {
+  return (
+    <AuthWrapper>
+      <div className="login-form">
+        <Form>
+          <label>
+            <ErrorMessage
+              name="username"
+              render={msg => <div className="error">{msg}</div>}
+            />
+            Username
+            <Field type="text" name="username" placeholder="Username" />
+          </label>
+          <label>
+            <ErrorMessage
+              name="password"
+              render={msg => <div className="error">{msg}</div>}
+            />
+            Password
+            <Field type="password" name="password" placeholder="Password" />
+          </label>
 
-                    <input type="submit"/>
-                </Form>
-            </div>
-        </AuthWrapper>
-    );
+          <input type="submit" />
+        </Form>
+      </div>
+    </AuthWrapper>
+  );
 }
 
 const LoginFormWithFormik = withFormik({
-    mapPropsToValues(){
-        return  {
-            username: "",
-            password: ""
-        };
-    },
+  mapPropsToValues() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
 
-    validationSchema: Yup.object().shape({
-        username: Yup.string()
-            .required("Please enter your Username"),
-        password: Yup.string()
-            .required("Please enter your Password")
-            .test("Length", "Length of your password should be between 12 and 30 characters", (val=> ((val.toString().length >= 12)&&(val.toString().length <= 30)) ? true : false)),
-    }),
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required("Please enter your Username"),
+    password: Yup.string()
+      .required("Please enter your Password")
+      .test(
+        "Length",
+        "Length of your password should be between 12 and 30 characters",
+        val =>
+          val && val.toString().length >= 12 && val.toString().length <= 30
+            ? true
+            : false
+      )
+  }),
 
-    handleSubmit(values, tools) {
-        // values: the values we get back from the form
-        // tools: some helpful methods we can use to interact with the form
-        
-        axios.post("URL HERE", values)
-          .then(res => {
-            console.log(res.data);
-            tools.resetForm();
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      }
+  handleSubmit(values, tools) {
+    // values: the values we get back from the form
+    // tools: some helpful methods we can use to interact with the form
+    tools.props.dispatch(
+      actions.login({ username: values.username, password: values.password })
+    );
+  }
 })(LoginForm);
 
-export default LoginFormWithFormik;
+const ConnectForm = connect()(LoginFormWithFormik);
+
+export default ConnectForm;
--- app/src/components/SignUp.jsx
+++ app/src/components/SignUp.jsx
@@ -1,79 +1,76 @@
-import React from 'react';
-import { withFormik, Form, Field,  ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
-import {AuthWrapper} from './styled-components';
+import React from "react";
+import { withFormik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { AuthWrapper } from "./styled-components";
 
-function SignUpForm(props){
-    console.log(props);
-    
-    return (
-        <AuthWrapper>    
-            <div className = "login-form">
-                <Form>
-                    <label>
-                        <ErrorMessage
-                            name = "username"
-                            render = {msg=> <div className = "error">{msg}</div>}
-                            />
-                        Username
-                        <Field
-                            type = "text"
-                            name = "username"
-                            placeholder = "Username"
-                            />
-                    </label>
-                    <label>
-                        <ErrorMessage
-                            name = "password"
-                            render = {msg=> <div className = "error">{msg}</div>}
-                            />
-                        Password
-                        <Field
-                            type = "password"
-                            name = "password"
-                            placeholder = "Password"
-                            />
-                    </label>
-                    <label>
-                        <ErrorMessage
-                            name = "confirm_password"
-                            render = {msg=> <div className = "error">{msg}</div>}
-                            />
-                        Confirm your Password
-                        <Field
-                            type = "password"
-                            name = "confirm_password"
-                            placeholder = "Confirm Password"
-                            />
-                    </label>
+function SignUpForm(props) {
+  console.log(props);
 
-                    <input type="submit"/>
-                </Form>
-            </div>
-        </AuthWrapper>
-    );
+  return (
+    <AuthWrapper>
+      <div className="login-form">
+        <Form>
+          <label>
+            <ErrorMessage
+              name="username"
+              render={msg => <div className="error">{msg}</div>}
+            />
+            Username
+            <Field type="text" name="username" placeholder="Username" />
+          </label>
+          <label>
+            <ErrorMessage
+              name="password"
+              render={msg => <div className="error">{msg}</div>}
+            />
+            Password
+            <Field type="password" name="password" placeholder="Password" />
+          </label>
+          <label>
+            <ErrorMessage
+              name="confirm_password"
+              render={msg => <div className="error">{msg}</div>}
+            />
+            Confirm your Password
+            <Field
+              type="password"
+              name="confirm_password"
+              placeholder="Confirm Password"
+            />
+          </label>
+
+          <input type="submit" />
+        </Form>
+      </div>
+    </AuthWrapper>
+  );
 }
 
 const SignUpFormWithFormik = withFormik({
-    mapPropsToValues(){
-        return  {
-            username: "",
-            password: "",
-            confirm_password: "",
-        };
-    },
+  mapPropsToValues() {
+    return {
+      username: "",
+      password: "",
+      confirm_password: ""
+    };
+  },
 
-    validationSchema: Yup.object().shape({
-        username: Yup.string()
-            .required("Please enter your Username"),
-        password: Yup.string()
-            .required("Please enter your Password")
-            .test("Length", "Length of your password should be between 12 and 30 characters", (val=> ((val.toString().length >= 12)&&(val.toString().length <= 30)) ? true : false)),
-        confirm_password: Yup.string()
-            .oneOf([Yup.ref('password'), null], "Passwords don't match")
-            .required('Password confirm is required')
-    })
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required("Please enter your Username"),
+    password: Yup.string()
+      .required("Please enter your Password")
+      .test(
+        "Length",
+        "Length of your password should be between 12 and 30 characters",
+        val =>
+          val.toString().length >= 12 && val.toString().length <= 30
+            ? true
+            : false
+      ),
+    confirm_password: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords don't match")
+      .required("Password confirm is required")
+  })
 })(SignUpForm);
 
 export default SignUpFormWithFormik;
