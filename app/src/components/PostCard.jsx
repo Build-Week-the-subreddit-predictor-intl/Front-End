4ebfdcaf55159a3b0a6927ab94635037fe5c9482
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-11-20 18:37:50 +0500

    Added Edit and delete buttons to posts

--- app/src/components/PostCard.jsx
+++ app/src/components/PostCard.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { PostCardWrapper } from "./styled-components";
+import {Link} from 'react-router-dom';
 
 export default function PostCard(props) {
   return (
     <PostCardWrapper className="post-card">
-      <div className="box">
-        <div className="box-header">
-          <img src="https://via.placeholder.com/80x80" />
-          {props.postData.title}
+      <Link to = {`/post-history/post${props.postData.id}`}>
+        <div className="box">
+          <div className="box-header">
+            <img src="https://via.placeholder.com/80x80" />
+            {props.postData.title}
+          </div>
+          <p className="box-body">{props.postData.text}</p>
         </div>
-        <p className="box-body">{props.postData.text}</p>
+      </Link>
+      <div className="button-container">
+        <button>Edit Post</button>
+        <button>Delete Post</button>
       </div>
     </PostCardWrapper>
   );
