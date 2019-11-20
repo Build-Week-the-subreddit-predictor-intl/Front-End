f71d8fba4e2fa6c325a2fdab11e0de98740e7cd4
Mark Halls <mark.halls@gmail.com>
2019-11-20 09:55:12 -0700

    Move sample data to reducer and pull data from state

--- app/src/components/PostCollection.jsx
+++ app/src/components/PostCollection.jsx
@@ -1,48 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+
 import PostCard from "./PostCard";
 
-export const previousPosts = [
-  {
-    title: "Lorem ipsum dolor sit amet",
-    text:
-      "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
-    id: 0
-  },
-  {
-    title: "Lorem ipsum dolor sit amet",
-    text:
-      "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
-    id: 1
-  },
-  {
-    title: "Lorem ipsum dolor sit amet",
-    text:
-      "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
-    id: 2
-  },
-  {
-    title: "Lorem ipsum dolor sit amet",
-    text:
-      "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
-    id: 3
-  },
-  {
-    title: "Lorem ipsum dolor sit amet",
-    text:
-      "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
-    id: 4
-  }
-];
 export default function PostCollection(props) {
+  const location = useLocation();
 
-  if (!previousPosts) {
+  console.log(location);
+  const posts = useSelector(state => state.posts);
+  if (!posts) {
     return <h2 className="loader">Loading...</h2>;
   } else {
     return (
       <div className="post-collection">
         <h2>Latest Posts</h2>
-        {previousPosts.map(post => {
+        {posts.map(post => {
           return <PostCard postData={post} key={post.id} />;
         })}
       </div>
