0a51bc8361ce9bbc09e9f17c974443cd861a09c2
Mark Halls <mark.halls@gmail.com>
2019-11-19 18:30:07 -0700

    Check if authorized on reddit

--- app/src/components/Dashboard.jsx
+++ app/src/components/Dashboard.jsx
@@ -1,82 +1,94 @@
-import React from 'react';
-import { withFormik, Form, Field,  ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
+import React from "react";
+import { withFormik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-import {DashboardWrapper} from './styled-components';
-import PostCollection from './PostCollection';
+import { DashboardWrapper } from "./styled-components";
+import PostCollection from "./PostCollection";
 
+function Dashboard(props) {
+  return (
+    <DashboardWrapper>
+      <div className="dashboard-form">
+        <Form>
+          <label>
+            <ErrorMessage
+              name="title"
+              render={msg => <div className="error">{msg}</div>}
+            />
+            Title
+            <Field
+              as="textarea"
+              type="text"
+              name="title"
+              placeholder="Post Title"
+            />
+          </label>
+          <label>
+            <ErrorMessage
+              name="text"
+              render={msg => <div className="error">{msg}</div>}
+            />
+            Text
+            <Field
+              as="textarea"
+              type="textarea"
+              name="text"
+              placeholder="Text (required)"
+            />
+          </label>
 
-function Dashboard(props){
-    return(
-        <DashboardWrapper>
-            <div className = "dashboard-form">
-                <Form>
-                    <label>
-                        <ErrorMessage
-                            name = "title"
-                            render = {msg=> <div className = "error">{msg}</div>}
-                            />
-                        Title
-                        <Field
-                            as = "textarea"
-                            type = "text"
-                            name = "title"
-                            placeholder = "Post Title"
-                            />
-                    </label>
-                    <label>
-                        <ErrorMessage
-                            name = "text"
-                            render = {msg=> <div className = "error">{msg}</div>}
-                            />
-                        Text
-                        <Field as = "textarea"
-                            type = "textarea"
-                            name = "text"
-                            placeholder = "Text (required)"
-                            />
-                    </label>
+          <button type="submit">Submit Post</button>
+        </Form>
+      </div>
 
-                    <button type="submit">Submit Post</button>
-                </Form>
-            </div>
-
-            <div className = "subreddits">
-                
-            </div>
-        </DashboardWrapper>
-    );
+      <div className="subreddits"></div>
+    </DashboardWrapper>
+  );
 }
 
 const DashboardWithFormik = withFormik({
-    mapPropsToValues(){
-        return  {
-            title: "",
-            text: ""
-        };
-    },
+  mapPropsToValues() {
+    return {
+      title: "",
+      text: ""
+    };
+  },
 
-    validationSchema: Yup.object().shape({
-        title: Yup.string()
-            .required("Your post needs to have a title"),
-        text: Yup.string()
-            .required("Your post needs to contain some text"),
-    }),
+  validationSchema: Yup.object().shape({
+    title: Yup.string().required("Your post needs to have a title"),
+    text: Yup.string().required("Your post needs to contain some text")
+  }),
 
-    handleSubmit(values, tools) {
-        // values: the values we get back from the form
-        // tools: some helpful methods we can use to interact with the form
-        
-        axios.post("URL HERE", values)
-          .then(res => {
-            console.log(res.data);
-            tools.resetForm();
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      }
+  handleSubmit(values, tools) {
+    // values: the values we get back from the form
+    // tools: some helpful methods we can use to interact with the form
+
+    axios
+      .post("URL HERE", values)
+      .then(res => {
+        console.log(res.data);
+        tools.resetForm();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 })(Dashboard);
 
-export default DashboardWithFormik;
+const DashBoardWithRedditAuth = () => {
+  const redditUrl = useSelector(state => state.redditAuthUrl);
+  const authed = useSelector(state => state.redditAuthed);
+  return (
+    <>
+      {authed ? (
+        <DashboardWithFormik />
+      ) : (
+        <a href={redditUrl}>Authorize with Reddit</a>
+      )}
+    </>
+  );
+};
+
+export default DashBoardWithRedditAuth;
